624e693752a6b524a05d8a810d9769a7cfc2f917
aLekSer <gdocse@gmail.com>
2015-02-06 04:40:24 +0300

    inverse codes

--- HalfMan.c
+++ HalfMan.c
@@ -15,14 +15,14 @@ typedef struct {
 } Node;
 typedef struct {
 	int len;
-	int seq;
+	unsigned int seq;
 } code;
 int compare(const Node ** left, const Node ** right)
 {
 	return ((*left)->val < (*right)->val) - ((*left)->val > (*right)->val);
 }
 void build_tree(Node** sorted, int len) {}
-void get_codes(code* arg_codes, Node** leaves)
+void get_inv_codes(code* arg_codes, Node** leaves)
 {
 	int i = 0;
 	Node * current, * upper;
@@ -43,8 +43,9 @@ void get_codes(code* arg_codes, Node** leaves)
 			{
 				codes[i].seq = codes[i].seq << 1;
 			}
+			codes[i].len ++;
 			//something goes wrong
-			if(upper->one != current && upper->zero != current)
+			if((upper->one != current) && (upper->zero != current))
 			{
 				abort();
 			}
@@ -59,7 +60,8 @@ int main(int argc, char* argv[])
 	int i , j, total;
 	int freq[SIZE] = {0};
 	Node *nodes[SIZE], *leaves[SIZE];
-	code codes[SIZE];
+	code codes[SIZE] = {0};
+	int set = 0;
 	Node * current = NULL;
 	read = fopen("D:\\working\\Small work for student\\HalfMan\\HalfMan\\Debug\\Tmp.txt", "r");
 	if(read)
@@ -97,12 +99,22 @@ int main(int argc, char* argv[])
 	}
 #endif
 	j = SIZE;
+	for(i = 0; i < SIZE; i++)
+	{
+		if (nodes[i]->val == 0 && !set)
+		{
+			j = i;
+			set = 1;
+		}
+		if(set)
+			nodes[i]->up = NULL;
+	}
 	while(j != 1)
 	{
 		--j;
-		//possibly wrong & operator
+		//possibly wrong & operator in &nodes[j-2], removed
 		if(j>=2)
-			qsort(&nodes[j-2], 3, sizeof(Node_p), compare);
+			qsort(nodes + j-2, 3, sizeof(Node_p), compare);
 		current = malloc(sizeof(Node));
 		nodes[j-1]->up = current;
 		nodes[j]->up = current;
@@ -119,12 +131,13 @@ int main(int argc, char* argv[])
 	{
 		printf("%d ", nodes[i]->val);
 	}
-	get_codes((code**) &codes, leaves);
+	get_inv_codes((code**) &codes, leaves);
 	printf("\nCodes: \n");
 	for(i = 0; i < SIZE; i++)
 	{
-		printf("%x ", codes[i].seq);
+		printf("s%d %x ", codes[i].len, codes[i].seq);
 	}
+	printf("\n%i \n", sizeof(codes[i].seq));
 
 	return 0;
 }
