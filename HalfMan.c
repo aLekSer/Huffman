400bb8394da47932a113ead8c12bf31cb322e68f
LekS <leks@epbyminw4272.minsk.epam.com>
2015-02-18 17:44:22 +0300

    Support of one symbol files added

--- HalfMan.c
+++ HalfMan.c
@@ -6,7 +6,7 @@
 #include <assert.h>
 #include <string.h>
 
-#define DEB1UG
+#define DE1BUG
 #define SIZE 256
 #define MARK -1
 
@@ -21,7 +21,7 @@ typedef struct {
 	uint64_t seq; //try to add array of long [8]
 } code;
 
-#define BUF_SIZE 8
+#define BUF_SIZE 1025
 //encode file
 void append_file( FILE * write, FILE * read , code* codes)
 {
@@ -196,11 +196,12 @@ void decode( FILE * encoded, element* tree, size_t tree_size, FILE * decoded, in
 				}
 				buf[buf_idx++] = tree[pos].letter;
 				pos = 0;
-			} */
+			}*/
 			break;
 		}
 	}
-	fwrite(buf, sizeof(char), file_size % BUF_SIZE, decoded);
+	//fwrite(buf, sizeof(char), file_size % BUF_SIZE, decoded);
+	fwrite(buf, sizeof(char), buf_idx+1, decoded);
 	fclose(decoded);
 }
 
@@ -327,6 +328,8 @@ int get_codes_from_array(element * prefix, code * codes)
 	}
 	return 0;
 }
+#define ARG_COMPRESS 1
+#define ARG_FILE 2
 int main(int argc, char* argv[])
 {
 	FILE * read, * write,  *write_dec, *read_enc;
@@ -337,6 +340,7 @@ int main(int argc, char* argv[])
 	unsigned int ui = 0;
 	Node *nodes[SIZE], *leaves[SIZE];
 	code codes[SIZE] = {{0}};
+	int is_zero_case = 0;
 	code codes2[SIZE] = {{0}};
 	uint64_t file_size = 0;
 	element prefix[2 * SIZE] = {{0}};
@@ -347,18 +351,18 @@ int main(int argc, char* argv[])
 	Node * current = NULL;
     if(argc == 4)
     {
-    	if (strcmp(argv[2], "-c") == 0)
+    	if (strcmp(argv[ARG_COMPRESS], "-c") == 0)
     	{
     		to_encode = 1; //TRUE
+    	    read = fopen(argv[ARG_FILE], "rb");
+    	    write = fopen(argv[3], "wb");
     	}
-    	if (strcmp(argv[2], "-x") == 0)
+    	if (strcmp(argv[ARG_COMPRESS], "-x") == 0)
     	{
     		to_encode = 0; //FALSE
+			read_enc = fopen(argv[ARG_FILE], "rb");
+    	    write_dec = fopen(argv[3], "wb");
     	}
-	    read = fopen(argv[1], "rb");
-	    write = fopen(argv[3], "wb");
-	    write_dec = fopen("Decode.txt", "wb");
-
     }
     else
     {
@@ -366,86 +370,108 @@ int main(int argc, char* argv[])
 	    write = fopen("D:\\working\\Small work for student\\HalfMan\\HalfMan\\Debug\\Encode.txt", "wb");
 	    write_dec = fopen("D:\\working\\Small work for student\\HalfMan\\HalfMan\\Debug\\Decode.txt", "wb");
     }
-	if(read)
-		while((c=fgetc(read)) != EOF)
+    if(to_encode)
+    {
+		if(read)
+			while((c=fgetc(read)) != EOF)
+			{
+				ui = (unsigned int) c;
+				freq[ui]++; file_size++;
+			}
+		for(i = 0; i < SIZE; i++)
 		{
-			ui = (unsigned int) c;
-			freq[ui]++; file_size++;
+			current = malloc(sizeof(Node));
+			current->up = NULL;
+			current->one = current->zero = NULL;
+			current->val = freq[i]; // this was to add randomization in tree
+			current->letter = i;
+			leaves[i] = current;
+			nodes[i] = current;
 		}
-	for(i = 0; i < SIZE; i++)
-	{
-		current = malloc(sizeof(Node));
-		current->up = NULL;
-		current->one = current->zero = NULL;
-		current->val = freq[i]; // this was to add randomization in tree
-		current->letter = i;
-		leaves[i] = current;
-		nodes[i] = current;
-	}
-	qsort(nodes, SIZE, sizeof(Node_p), compare);
+		qsort(nodes, SIZE, sizeof(Node_p), compare);
 
-	j = SIZE;
-	for(i = 0; i < SIZE; i++)
-	{
-		if (nodes[i]->val == 0 && !set)
+		j = SIZE;
+		for(i = 0; i < SIZE; i++)
 		{
-			j = i;
-			set = 1;
+			if (nodes[i]->val == 0 && !set)
+			{
+				j = i;
+				set = 1;
+			}
+			if(set)
+				nodes[i]->up = NULL;
 		}
-		if(set)
-			nodes[i]->up = NULL;
-	}
-	while(j != 1)
-	{
-		--j;
-		if(j>=2)
-			qsort(nodes, j+1, sizeof(Node_p), compare);
-		current = malloc(sizeof(Node));
-		nodes[j-1]->up = (Node_p)current;
-		nodes[j]->up = (Node_p)current;
-		current->val = nodes[j]->val + nodes[j-1]->val;
-		current->zero = (Node_p)nodes[j];
-		current->one = (Node_p)nodes[j-1];
-		nodes[j-1] = current;
-		nodes[j] = NULL;//->val = MARK;
-	}
-	// this is a root node
-	nodes[0]->up = NULL;
-	put_to_array(prefix, nodes[0], -1);
-	code next;
-	next.seq = -1;
-	//search_for_codes(prefix, codes, 0, &next);
-	//get_codes_from_array(prefix, codes);
-
-	printf("\nPrefix: \n");
-	for(i = 0; i < 2*SIZE; i++)
-	{
-		if (prefix[i].letter != 0)
-			printf("%d ",(char) prefix[i].letter, codes[i].len, codes[i].seq);
-	}
-	printf("\nCodes2: \n");
-	get_inv_codes(codes2, leaves);
-	for(i = 0; i < SIZE; i++)
-	{
-		if (codes2[i].len != 0)
-			printf("%c s%d  %x ",(char) i, codes2[i].len, codes2[i].seq);
-	}
-	printf("\n");
-	write_file(write, prefix, file_size);
-	if(argc == 4)
-	{
-		read = fopen(argv[1], "rb");
-		read_enc = fopen(argv[3], "rb");
-	}
-	else
-	{
-	    read = fopen("D:\\working\\Small work for student\\HalfMan\\HalfMan\\Debug\\Tmp.txt", "rb");
-	    read_enc = fopen("D:\\working\\Small work for student\\HalfMan\\HalfMan\\Debug\\Encode.txt", "rb");
-	}
-	append_file(write, read, codes2);
-	read_prefix(read_enc, dprefix, &tree_size, & file_size);
-	decode(read_enc, dprefix, tree_size, write_dec, file_size);
-	Free_Tree(&(nodes[0]));
+		//for one letter cases corner case
+		if(j==1)
+		{
+			current = malloc(sizeof(Node));
+			nodes[0]->up = (Node_p)current;
+			current->one = (Node_p)nodes[0];
+			nodes[0] = current;
+		}
+		while(j != 1)
+		{
+			--j;
+			if(j>=2)
+				qsort(nodes, j+1, sizeof(Node_p), compare);
+			current = malloc(sizeof(Node));
+			nodes[j-1]->up = (Node_p)current;
+			nodes[j]->up = (Node_p)current;
+			current->val = nodes[j]->val + nodes[j-1]->val;
+			current->zero = (Node_p)nodes[j];
+			current->one = (Node_p)nodes[j-1];
+			nodes[j-1] = current;
+			nodes[j] = NULL;//->val = MARK;
+		}
+		// this is a root node
+		nodes[0]->up = NULL;
+		put_to_array(prefix, nodes[0], -1);
+		/*code next;
+		next.seq = -1;
+		search_for_codes(prefix, codes, 0, &next);*/
+		//get_codes_from_array(prefix, codes);
+		get_inv_codes(codes2, leaves);
+#ifdef DEBUG
+		printf("\nPrefix: \n");
+		for(i = 0; i < 2*SIZE; i++)
+		{
+			if (prefix[i].letter != 0)
+				printf("%d ",(char) prefix[i].letter, codes[i].len, codes[i].seq);
+		}
+		printf("\nCodes2: \n");
+		for(i = 0; i < SIZE; i++)
+		{
+			if (codes2[i].len != 0)
+				printf("%c s%d  %x ",(char) i, codes2[i].len, codes2[i].seq);
+		}
+		printf("\n");
+#endif
+		if(argc == 4)
+		{
+			read = fopen(argv[ARG_FILE], "rb");
+		}
+		else
+		{
+			read = fopen("D:\\working\\Small work for student\\HalfMan\\HalfMan\\Debug\\Tmp.txt", "rb");
+		}
+		write_file(write, prefix, file_size);
+		append_file(write, read, codes2);
+		Free_Tree(&(nodes[0]));
+    }
+    else
+    {
+		if(argc != 4)
+		{
+			read_enc = fopen("D:\\working\\Small work for student\\HalfMan\\HalfMan\\Debug\\Encode.txt", "rb");
+		}
+		if(read_enc)
+		{
+			read_prefix(read_enc, dprefix, &tree_size, & file_size);
+			decode(read_enc, dprefix, tree_size, write_dec, file_size);
+		}
+		else
+			assert(0);
+    }
 	return 0;
 }
 
